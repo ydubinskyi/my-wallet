37fcee772187f81a7e1a528bd3e85e56d5430b08
ydubinskyi <slavabilaz@gmail.com>
2019-10-13 22:19:14 +0200

    fixed test db config

--- apps/api/src/app/db.module.ts
+++ apps/api/src/app/db.module.ts
@@ -1,18 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as path from 'path';
-
-import { Currency } from './currency/currency.entity';
-import { User } from './user/user.entity';
-import { Account } from './account/account.entity';
 
 @Module({
   imports: [
-    // TODO: Find workaround for entities and migrations problem
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: `${path.resolve(__dirname)}/wallet-db.sqlite`,
-      entities: [Currency, User, Account],
+      database: `wallet-db.sqlite`,
+      entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
   ],
