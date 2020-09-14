4d4b9ff5a777b0da4bef879b51b80e86d8694c13
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2020-09-14 15:08:06 +0200

    removed useless files and placeholders

---
+++ apps/client/src/app/components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <>
+      <header></header>
+      <main>
+        <p>Main content</p>
+      </main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default Layout;
