580f2b2ba82406de17e2e8e867754cc189d5418f
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2020-09-20 22:48:48 +0200

    add ids to seeds

--- apps/api/src/app/db/seeds/01_currencies.ts
+++ apps/api/src/app/db/seeds/01_currencies.ts
@@ -7,26 +7,31 @@ export async function seed(knex: Knex) {
 
   await knex(TABLES.CURRENCY).insert([
     {
+      id: 101,
       name: 'US Dollar',
       code: 'USD',
       symbol: '$',
     },
     {
+      id: 102,
       name: 'Euro',
       code: 'EUR',
       symbol: '€',
     },
     {
+      id: 103,
       name: 'British Pound',
       code: 'GBP',
       symbol: '£',
     },
     {
+      id: 104,
       name: 'Polish Zloty',
       code: 'PLN',
       symbol: 'PLN',
     },
     {
+      id: 105,
       name: 'Ukrainian hryvna',
       code: 'UAH',
       symbol: 'UAH',
